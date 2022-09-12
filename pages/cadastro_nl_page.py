d7b50d4cddf2ee7d7dd0acf9ea0f27ec6de2148b
nedsonicaro <icaronedson54@gmail.com>
2022-09-12 13:48:01 -0300

    Criação de classe cadastro com funcao de escrever CPF

---
+++ pages/cadastro_nl_page.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from browser import Browser
+
+
+class NLCadastroPageLocator(object):
+    # TODO Escrever os locators necessários para realizar o login na pagina NL
+    INPUT_CPF = "id_sc_field_login"
+    INPUT_SENHA = "id_sc_field_pswd"
+    INPUT_CONFIRMA_SENHA = "id_sc_field_confirm_pswd"
+    INPUT_EMAIL = "id_sc_field_email"
+    INPUT_CADASTRAR = "//span[.='Cadastrar']"
+
+
+class NLCadastroPage(Browser):
+    # def escrever_email(self):
+    # TODO Escreve as funções da page. Para os meninos do teste...
+
+    def acessar_Nl(self, url):
+        self.driver.get(url)
+
+    def escrever_cpf(self, cpf):
+        inputCPF = self.driver.find_element(
+            By.ID, NLCadastroPageLocator.INPUT_CPF)
+        inputCPF.send_keys(cpf)
--- pages/login_nl_page.py
+++ pages/login_nl_page.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support import expected_conditions as ec
 from browser import Browser
 
 class NLLoginPageLocator(object):
-    # TODO Escrever os locators nescessário para realizar o ligin na pagina NL
+    # TODO Escrever os locators necessários para realizar o login na pagina NL
     INPUT_CPF = "id_sc_field_login"
     INPUT_SENHA = "id_sc_field_pswd"
     CLICK_ACESSAR = "//div/input[@class='button']"
