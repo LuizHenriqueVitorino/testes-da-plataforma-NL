78dc5f59bb2b986cea49a9c1d071977fc995e52e
ViniciusAndradex <viniciusandradeprog@gmail.com>
2022-09-12 13:53:40 -0300

    todas os testes para cpfforam criadas. CPF: branco, valido e invalido.

--- features/cadastro_nl_valido.feature
+++ features/cadastro_nl_valido.feature
@@ -1,12 +1,12 @@
 # language: pt
 
-Funcionalidade: Cadastrar um usuário na plataforma NL
+Funcionalidade: Testar o sistema de cadastro da página NL
 
     Contexto: acessar página de teste
         Dado que acessa a seção de cadastro da página NL
 
     Cenário: acessar pagina NL e realizar cadastro de um usuário
-        Dado que preenche o campo cpf
+        Dado que preenche o campo cpf de forma valida
         Dado que preenche o campo senha
         Dado que preenche o campo confirme a senha
         Dado que preenche o campo e-mail
--- steps/cadastro_nl_steps.py
+++ steps/cadastro_nl_steps.py
@@ -4,32 +4,44 @@ from browser import Browser
 
 @given(u'que acessa a seção de cadastro da página NL')
 def step_impl(context):
-    # NLPC.nl_cadastro(context.url)
+    # NLPC.nl_cadastro(url)
     pass
-@given(u'que preenche o campo cpf')
+
+@given(u'que preenche o campo cpf de forma invalida')
 def step_impl(context):
     # NLPC.nl_escrever_cpf(cpf)
     pass
+
+@given(u'que preenche o campo cpf de forma valida')
+def step_impl(context):
+    # NLPC.nl_escrever_cpf(cpf)
+    pass
+
 @given(u'que preenche o campo cpf com branco')
 def step_impl(context):
     # NLPC.nl_escrever_cpf(''/ pode ser um boolean em falso para preencher com branco)
     pass
+
 @given(u'que preenche o campo senha')
 def step_impl(context):
     # NLPC.nl_escrever_senha(senha)
     pass
+
 @given(u'que preenche o campo confirme a senha')
 def step_impl(context):
     # NLPC.nl_escrever_confirme_senha(senha)
     pass
+
 @given(u'que preenche o campo e-mail')
 def step_impl(context):
     # NLPC.nl_escrever_email(email)
     pass
+
 @when(u'clico no botão cadastrar')
 def step_impl(context):
     # NLPC.nl_clicar_cadastrar()
     pass
+
 @then(u'devo passar para o preenchimento dos próximos dados')
 def step_impl(context):
     # assert Browser.driver.title == 'Inclusão - nl_Pessoa'
@@ -38,5 +50,4 @@ def step_impl(context):
 @then(u'devo falhar no cadastro')
 def step_impl(context):
     # assert Browser.driver.title != 'Inclusão - nl_Pessoa'
-    pass
-    
+    pass 
