8dec43f1b3803f6040556dd2bf5f338b2b2e14a4
ViniciusAndradeX <viniciusandradeprog@gmail.com>
2022-09-16 22:30:56 -0300

    opções

--- pages/pesquisador_nl_page.py
+++ pages/pesquisador_nl_page.py
@@ -14,44 +14,51 @@ class NLPesquisadorPageLocator(object):
     ID_SEUS_NUMEROS = 'item_54'
     ID_DASHBOARD = 'item_66'
     ID_FECHAR_DASHBOARD = 'aba_td_img_item_item_66'
+    ID_ESPAÇO_BRANCO = 'lin1_col2'
 
 class NLPesquisadorPage(Browser):
-    
-    def acessar_alterar_dados(self):
+
+    def clicar_branco(self):
+        self.driver.find_element(By.ID, NLPesquisadorPageLocator.ID_ESPAÇO_BRANCO).click()
+
+    def pesquisador(self):
         pesquisador = self.driver.find_element(By.ID, NLPesquisadorPageLocator.ID_PESQUISADOR)
         pesquisador.click()
+    
+    def acessar_alterar_dados(self):
+        self.pesquisador()
         alterar_dados = self.driver.find_element(By.ID, NLPesquisadorPageLocator.ID_ALTERAR_DADOS)
         alterar_dados.click()
 
     def acessar_alterar_senha(self):
-        pesquisador = self.driver.find_element(By.ID, NLPesquisadorPageLocator.ID_PESQUISADOR)
-        pesquisador.click()
+        self.pesquisador()
         alterar_senha = self.driver.find_element(By.ID, NLPesquisadorPageLocator.ID_ALTERAR_SENHA)
         alterar_senha.click()
 
     def acessar_mensagem_sistema(self):
-        pesquisador = self.driver.find_element(By.ID, NLPesquisadorPageLocator.ID_PESQUISADOR)
-        pesquisador.click()
+        self.pesquisador()
         mensagem_sistema = self.driver.find_element(By.ID, NLPesquisadorPageLocator.ID_MENSAGEM_SISTEMA)
         mensagem_sistema.click()
 
     def acessar_conversor_lattes(self):
-        pesquisador = self.driver.find_element(By.ID, NLPesquisadorPageLocator.ID_PESQUISADOR)
-        pesquisador.click()
-        conversor_lattes = self.driver.find_element(By.ID, NLPesquisadorPageLocator.ID_CONVERSOR_LATTES)
-        conversor_lattes.click()
-        
-    def acessar_conversor_lattes(self):
-        pesquisador = self.driver.find_element(By.ID, NLPesquisadorPageLocator.ID_PESQUISADOR)
-        pesquisador.click()
+        self.pesquisador()
         conversor_lattes = self.driver.find_element(By.ID, NLPesquisadorPageLocator.ID_CONVERSOR_LATTES)
         conversor_lattes.click()
         
     def acessar_dashoard(self):
-        pesquisador = self.driver.find_element(By.ID, NLPesquisadorPageLocator.ID_PESQUISADOR)
-        pesquisador.click()
+        self.pesquisador()
         seus_numeros = self.driver.find_element(By.ID, NLPesquisadorPageLocator.ID_SEUS_NUMEROS)
         seus_numeros.click()
         dashboard = self.driver.find_element(By.ID, NLPesquisadorPageLocator.ID_DASHBOARD)
         dashboard.click()
-        
+
+    def fechar_aba(self, fechar_alterar_dados=False, fechar_alterar_senha=False, fechar_mensagem_sistema=False, fechar_dashboard=False):
+        if fechar_alterar_dados:
+            self.driver.find_element(By.ID, NLPesquisadorPageLocator.ID_FECHAR_ALTERAR_DADOS).click()
+        if fechar_alterar_senha:
+            self.driver.find_element(By.ID, NLPesquisadorPageLocator.ID_FECHAR_ALTERAR_SENHA).click()
+        if fechar_mensagem_sistema:
+            self.driver.find_element(By.ID, NLPesquisadorPageLocator.ID_FECHAR_MENSAGEM_SISTEMA).click()
+        if fechar_dashboard:
+            self.driver.find_element(By.ID, NLPesquisadorPageLocator.ID_FECHAR_DASHBOARD).click()
+                      
--- steps/pesquisador_nl_steps.py
+++ steps/pesquisador_nl_steps.py
@@ -1,6 +1,8 @@
+from ast import Assert
 from behave import *
 from login import Login
 from pages.pesquisador_nl_page import NLPesquisadorPage
+from selenium.webdriver.common.by import By
 
 obj_login = Login()
 obj_pesquisador = NLPesquisadorPage()
@@ -14,8 +16,12 @@ def step_impl(context):
 @given(u'que acessa alterar seus dados')
 def step_impl(context):
     obj_pesquisador.acessar_alterar_dados()
+    obj_pesquisador.clicar_branco()
 
 
 @then(u'o titulo da aba deve ser igual ao do menu')
 def step_impl(context):
-    pass
+    assert obj_login.CPF in obj_pesquisador.driver.find_element(By.ID, 'lin1_col2').text
+    # assert obj_pesquisador.driver.find_element(By.ID, 'aba_td_item_5').text == 'Alterar seus Dados'
+    # obj_pesquisador.driver.find_element(By.ID, 'item_5')
+    obj_pesquisador.fechar_aba(fechar_alterar_dados=True)
