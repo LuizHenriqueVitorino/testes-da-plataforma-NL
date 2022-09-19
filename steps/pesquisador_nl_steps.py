61d02cd11b1890ce908e94ade848d8296ddf1a74
ViniciusAndradeX <viniciusandradeprog@gmmail.com>
2022-09-19 14:20:22 -0300

    Tentando ajustar o teste para que funcione

--- steps/pesquisador_nl_steps.py
+++ steps/pesquisador_nl_steps.py
@@ -11,15 +11,13 @@ def step_impl(context):
     obj_login.acessar_Nl('https://prpi.ifce.edu.br/nl/app_Login/')
     obj_login.logar()
 
-
 @given(u'que acessa alterar seus dados')
 def step_impl(context):
     obj_pesquisador.acessar_alterar_dados()
     obj_pesquisador.clicar_branco()
-    
 
 @then(u'o campo cpf deve ser igual ao de login')
 def step_impl(context):
     assert 'Alterar seus Dados' == obj_pesquisador.driver.find_element(By.ID,'aba_td_item_5').text
-    assert obj_pesquisador.driver.find_element(By.CSS_SELECTOR, '#id_read_on_cpf').text == 'cpf'
+    assert obj_pesquisador.driver.find_element(By.ID, 'id_read_on_cpf').text == obj_login.CPF
     obj_pesquisador.fechar_aba(fechar_alterar_dados=True)
